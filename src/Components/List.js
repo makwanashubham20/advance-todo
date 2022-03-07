3bbf0751242f09853b24c0ed897c3bf700da37d9
Shubham <shubhammakwana53@gmail.com>
2022-03-07 15:28:41 +0530

    A small fix for drag and drop

--- src/Components/List.js
+++ src/Components/List.js
@@ -1,7 +1,7 @@
 function List({ item, deleteTask, CompleteTask, dragHandler, dropHandler }) {
 
     return (
-        <li key={item.key} id={item.key} draggable={true} onDragOver={(ev) => ev.preventDefault()} onDragStart={dragHandler} onDrop={dropHandler}>
+        <li key={item.key} id={item.key} draggable={true} onDragOver={(ev) => ev.preventDefault()} onDragStart={dragHandler} onDragEnter={dropHandler}>
             <input type="checkbox" className="Completed" checked={item.isCompleted} onChange={() => CompleteTask(item.key)} />
             <span className="editable" onClick={() => CompleteTask(item.key)}>{item.isCompleted ? (<s>{item.task}</s>) : <>{item.task}</>}</span>
             <button onClick={() => deleteTask(item.key)} id="delete-task">X</button>
